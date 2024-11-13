89777d39fca63125a4fadb6f25f3d6be163afebc
Mubashir Hassan <mhm13dev@gmail.com>
2024-11-13 23:35:37 +0500

    feat: add error handling

--- app/_components/users-section.tsx
+++ app/_components/users-section.tsx
@@ -8,39 +8,58 @@ interface Props {
 }
 
 export const UsersSection: React.FC<Props> = async ({ searchParams }) => {
-  const { users, hasMore, timeTaken } = await getUsers({
-    page: searchParams.page,
-    size: searchParams.size,
-    search: searchParams.search,
-  });
+  try {
+    const { users, hasMore, timeTaken } = await getUsers({
+      page: searchParams.page,
+      size: searchParams.size,
+      search: searchParams.search,
+    });
 
-  return (
-    <>
+    return (
+      <>
+        <section className="my-8">
+          <p className="text-gray-500 text-sm mb-2 text-right">
+            Time taken: {timeTaken}ms
+          </p>
+
+          <div className="space-y-4">
+            {users.map((user) => (
+              <div
+                key={user.id}
+                className="p-4 bg-background text-foreground rounded-lg shadow"
+              >
+                <p className="font-medium text-gray-800">
+                  {user.id}. {user.first_name} {user.last_name}
+                </p>
+                <p className="mt-0.5 text-xs text-gray-500">{user.email}</p>
+              </div>
+            ))}
+
+            {users.length === 0 && (
+              <p className="text-center text-gray-500">No users found.</p>
+            )}
+          </div>
+        </section>
+
+        <PaginationSection searchParams={searchParams} hasMore={hasMore} />
+      </>
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return (
+        <section className="my-8">
+          <p className="bg-red-500/20 px-4 py-2 rounded text-red-500/80 text-sm text-center">
+            {error.message}
+          </p>
+        </section>
+      );
+    }
+    return (
       <section className="my-8">
-        <p className="text-gray-500 text-sm mb-2 text-right">
-          Time taken: {timeTaken}ms
+        <p className="bg-red-500/20 px-4 py-2 rounded text-red-500/80 text-sm text-center">
+          An unknown error occurred.
         </p>
-
-        <div className="space-y-4">
-          {users.map((user) => (
-            <div
-              key={user.id}
-              className="p-4 bg-background text-foreground rounded-lg shadow"
-            >
-              <p className="font-medium text-gray-800">
-                {user.id}. {user.first_name} {user.last_name}
-              </p>
-              <p className="mt-0.5 text-xs text-gray-500">{user.email}</p>
-            </div>
-          ))}
-
-          {users.length === 0 && (
-            <p className="text-center text-gray-500">No users found.</p>
-          )}
-        </div>
       </section>
-
-      <PaginationSection searchParams={searchParams} hasMore={hasMore} />
-    </>
-  );
+    );
+  }
 };
